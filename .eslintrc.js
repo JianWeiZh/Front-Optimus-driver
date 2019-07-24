542e764c8a0f08a68bea50a802414ee1c3bd3165
张健伟 <zjw393854452@163.com>
2019-07-24 14:51:00 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'no-multiple-empty-lines': 'error',
+    'no-multi-spaces': 'error',
+    'no-extra-bind': 'error',
+    'no-eval': 'error',
+    'eqeqeq': 'error',
+    'indent': ['error', 2],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import './cube-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+import Navigation from 'vue-navigation'
+
+
+
+
 
 Vue.config.productionTip = false
+Vue.use(Navigation, { router })
+
+
+
+
 
 new Vue({
   router,
