269e303f56cb65a8ba3e3954b0da60a97cf8c394
张健伟 <zjw393854452@163.com>
2019-07-24 18:19:57 +0800

    增加备注

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,26 @@ const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
   productionSourceMap: false,
-  configureWebpack: config => {
-    config.plugins.push(new CompressionWebpackPlugin({
-      algorithm: 'gzip',
-      test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
-      threshold: 10240,
-      minRatio: 0.8
-    }))
+  chainWebpack: config => {
+    console.log(NODE_ENV)
+    if (NODE_ENV === 'production') {
+      config
+        .plugin('compression')
+        .use(CompressionWebpackPlugin, {
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+        .tap(args => {
+          return [/* 传递给 html-webpack-plugin's 构造函数的新参数 */]
+        })
+    }
+    config.externals({
+      'vue': 'Vue',
+      'vuex': 'Vuex',
+      'vue-router': 'VueRouter'
+    })
   },
   css: {
     sourceMap: NODE_ENV !== 'product',
